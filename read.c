f9cf5746ad4ce8e2c59271feb17ffcfe4e19887e
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-28 14:57:10 +0530

    depicting dynamic allocation of device numbers

---
+++ read.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<fcntl.h>
+
+#define SIZE 20
+
+int main()
+{
+        int fd,ret;
+        char buf[SIZE];
+
+        fd = open("/dev/test0", O_RDWR);
+
+        if(fd<0)
+        {
+                perror("Error opening the device\n");
+                return -1;
+        }
+        read(fd,buf,SIZE);
+
+        printf("data read is %s\n",buf);
+        return 0;
+}
+
+
+
+
+
+
+
+
---
+++ write.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<fcntl.h>
+#define SIZE 20
+int main()
+{
+
+        int fd,ret;
+        char buf[SIZE]="Hello DESD\n";
+
+        fd=open("/dev/test0",O_RDWR);
+        if(fd<0)
+        {
+                perror("Error operation\n");
+        return -1;
+        }
+
+         write(fd,buf,sizeof(buf));
+        return 0;
+}
+
+
